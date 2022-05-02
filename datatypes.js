a3d734a2625f63b9efce361db25d2865d74d9ec7
tenzinlotey <teeonextenzin@gmail.com>
2022-05-02 23:02:23 +0530

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+// console.log(name)
+// var name:string;
+var name;
+name = "mohan";
+var message = 'hi,\
+my name is $(name)';
+console.log(message);
+// Generic Array
+var Name;
+var number = new Number();
+number = 40;
+console.log(number);
+var age = 23;
+console.log(age);
+var list = ['apple', 'mango'];
+console.log(list);
+// Generic aarray
+var anyList;
+anyList = ['hi', 32, true];
+console.log(anyList);
+// Tuple datatypes
+var tupArray = ['hi', 34, 'mohan', 87];
+console.log(tupArray);
+var multiArray = ["hi", 34, "sonam", 56];
+console.log(multiArray);
---
+++ datatypes.ts
@@ -0,0 +1,29 @@
+// null,undefined,number,string,boolean,any
+export{}
+// console.log(name)
+// var name:string;
+var name:string ;
+name ="mohan";
+var message:string = 'hi,\
+my name is $(name)';
+console.log(message)
+// Generic Array
+var Name:Array<string> ;
+var number = new Number()
+number = 40
+console.log(number)
+var age:number = 23;
+console.log(age)
+var list:string [] = ['apple','mango']
+console.log(list)
+// Generic aarray
+var anyList:Array<any>;
+anyList= ['hi',32,true]
+console.log(anyList)
+
+// Tuple datatypes
+var tupArray:[string , number,string,number] = ['hi',34,'mohan',87];
+console.log(tupArray)
+
+var multiArray:Array<string|number> = ["hi",34,"sonam",56]
+console.log(multiArray)
